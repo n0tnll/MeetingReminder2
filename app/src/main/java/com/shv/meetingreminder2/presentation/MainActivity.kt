c97f00022d8cfdb6df8e1200e183663b7f334e7a
Sergey <frostfreerun@gmail.com>
2024-01-06 15:24:46 +1000

    Set theme

--- app/src/main/java/com/shv/meetingreminder2/presentation/MainActivity.kt
+++ app/src/main/java/com/shv/meetingreminder2/presentation/MainActivity.kt
@@ -23,9 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         checkPermission()
-
         setupToolBar()
     }
 
